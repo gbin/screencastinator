bf59baf22d1181b53def8ad4c0e7481ffc965b18
Guillaume BINET <gbin@gootz.net>
2013-01-23 12:07:04 -0800

    missing file

---
+++ src/scriptedit/editor.go
@@ -0,0 +1,118 @@
+package scriptedit
+
+import (
+	"bufio"
+	"fmt"
+)
+
+type Timing struct {
+	Time   float32
+	Length int
+}
+
+type EditorState struct {
+	Content      []AnsiCmd // The parsed graphical content
+	Timings      []Timing  // The associated timings to play them
+	Position     int       // The current index in the Content
+	Bytepos      int       // The raw offset corresponding to the Position
+	Time         float32   // The time correspoding to the Position
+	Total_time   float32   // The total time of the replay
+	In		     int       // The IN marker
+	Out          int       // The OUT marker
+}
+
+func NewEditorState() *EditorState {
+	state := new(EditorState)
+	state.In = -1
+	state.Out = -1
+	return state
+}
+
+func (state *EditorState) Bytepos2position(bytepos int) int {
+	var offset int
+	for index, ansi := range state.Content {
+		offset+= len(ansi.String())
+		if offset >= bytepos {
+			return index
+		}
+	}
+	return -1
+}
+
+func (state *EditorState) NextTiming() bool {
+	timeindex, offset, _ := state.deduceTiming(state.Bytepos)
+	if timeindex < len(state.Timings) - 1 {
+		state.Position = state.Bytepos2position(offset + state.Timings[timeindex + 1].Length)
+		return true // position changed
+	}
+	return false
+}
+
+func (state *EditorState) PreviousTiming() bool {
+	timeindex, offset, _ := state.deduceTiming(state.Bytepos)
+	if state.Bytepos > offset {
+		state.Position = state.Bytepos2position(offset)
+		return true
+	}
+	if timeindex > 0 {
+		state.Position = state.Bytepos2position(state.Bytepos - state.Timings[timeindex - 1].Length)
+		return true
+	}
+	return false
+}
+
+func (state *EditorState) DeleteRegion(from, to int) bool {
+	for i := from; i < to; i++ {
+		// FIXME
+//		bytesToRemove := len([]byte(state.Content[i].String()))
+//		timeindex, _, _ := state.deduceTiming(state.Bytepos)     // FIXME we should look if it is at the end or at the beginning of the block
+//		if state.Timings[timeindex].Length >= bytesToRemove {
+//			state.Timings[timeindex].Length-= bytesToRemove
+//		} else {
+//			bytesToRemove-= state.Timings[timeindex].Length
+//			state.Timings[timeindex].Length = 0
+//			state.Timings[timeindex + 1].Length -= bytesToRemove  // FIXME, we should recurse here
+//
+//		}
+	}
+
+	copy(state.Content[from:], state.Content[to:])
+	state.Content = state.Content[:len(state.Content) - (to - from)]
+	_, _, state.Time = state.deduceTiming(state.Bytepos)
+
+	return false
+
+}
+
+
+func (state *EditorState) deduceTiming(position int) (int, int, float32) {
+	var time float32
+	var index int
+	for pos , t := range state.Timings {
+		if index >= position {
+			return pos, index, time
+		}
+		time += t.Time
+		index += t.Length
+	}
+	return len(state.Timings) - 1 , index, time
+}
+
+func (state *EditorState) ParseTimings(reader *bufio.Reader) {
+	state.Timings = make([]Timing, 0)
+
+	// Put an artificial 0 at the beginning
+	state.Timings = append(state.Timings, Timing{0, 0})
+
+	for true {
+		var line, err = reader.ReadBytes('\n')
+		var entry Timing = Timing{0, 0}
+		if err != nil {
+			break
+		}
+		fmt.Sscanf(string(line), "%f %d", &entry.Time, &entry.Length)
+		state.Timings = append(state.Timings, entry)
+	}
+	_, _, state.Total_time = state.deduceTiming(len(state.Content) - 1)
+
+}
